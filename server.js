91207f564147f62336ef615c2b853c079e525d1c
rémi  <remirobert33530@gmail.com>
2015-12-30 12:11:48 +0800

    add server route post upload

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ var resumable = require('resumable');
 var multipart = require('connect-multiparty');
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+app.use(multipart());
+
+app.post('/upload', function(req, res){
+  resumable.post(req, function(status, filename, original_filename, identifier){
+    res.send(status);
+  });
+});
+
 var server = app.listen(3000, function () {
   console.log('server stared');
 });
