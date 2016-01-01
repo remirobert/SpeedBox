6df3e963e94027672e8c946830bb86bd5afd6ed7
rémi  <remirobert33530@gmail.com>
2016-01-01 16:26:53 +0800

    add upload client file stream

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-
+var ss = require('socket.io-stream');
+var path = require('path');
+var fs = require('fs');
 
 io.on('connection', function (socket) {
   console.log("user connected");
   socket.emit('welcome', 'hello world');
+
+  ss(socket).on('profile-image', function(stream, data) {
+      var filename = path.basename(data.name);
+      stream.pipe(fs.createWriteStream(filename));
+  });
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
