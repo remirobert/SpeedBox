51424525a6995cf190ad6e8ed4ddf8cfd18e1c19
Flavien Pensato <Flavien Pensato>
2016-01-14 16:44:41 +0800

    Change run.bat meteor

--- App meteor/run.bat
+++ App meteor/run.bat
@@ -1,6 +1,2 @@
-net start MongoDB
 set MONGO_URL=mongodb://localhost:27017/yourDatabaseName
 meteor --settings settings.json --port 5000
-
-cd C:\Users\Flavien\Source\Repos\SpeedBox-Meteor
-pm2 start pm2.json
